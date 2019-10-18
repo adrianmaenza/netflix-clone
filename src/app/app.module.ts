42c2f888e6d6c69f38111ba93b856aec6882d413
Adrian Maenzanise <adrian@slickdesigns4u.com>
2019-10-18 13:21:20 +0200

    Added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieSingleComponent } from './components/movie-single/movie-single.component';
+import { MovieAddComponent } from './components/movie-add/movie-add.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieSingleComponent,
+    MovieAddComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
